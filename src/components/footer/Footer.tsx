0ab305c72ade0810ee89d9b027b3a7b0715b823e
PhanHung <phanquochung280702@gmail.com>
2023-03-05 15:48:59 +0700

    add couting up and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import FooterTop from './components/FooterTop'
 import "./footer.scss"
+
 
 export default function Footer(): JSX.Element {
     return (
-        <div className='footer'>Footer</div>
+        <div className='footer'>
+            <FooterTop />
+            <div className="footer_bottom">
+                <div className="title">
+                    <p>Copyright <i className="fa-regular fa-copyright"></i> 2021. All rights reserved.</p>
+                </div>
+                <div className="content">
+                    <i className="fa-brands fa-facebook-f"></i>
+                    <i className="fa-brands fa-instagram"></i>
+                    <i className="fa-brands fa-twitter"></i>
+                </div>
+            </div>
+        </div>
     )
 }
