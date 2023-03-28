5fdc92e349d63cf4112c423481541387b399520d
PhanHung <phanquochung280702@gmail.com>
2023-03-28 15:02:23 +0700

    fix courseCatalog add backToTop

--- src/layouts/home/HomeLayout.tsx
+++ src/layouts/home/HomeLayout.tsx
@@ -3,14 +3,14 @@ import { Outlet } from "react-router-dom";
 import BackToTopButton from "../../components/back-to-top/BackToTopButton";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
-import "./homeLayout.scss"
+
 export default function HomeLayout(): JSX.Element {
   return (
-    <div className="home_layout">
+    <>
       <Header />
       <Outlet />
       <BackToTopButton />
       <Footer />
-    </div>
+    </>
   );
 }
